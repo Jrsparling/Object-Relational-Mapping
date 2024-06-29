c576dccd07be44f17818fa68583456999d914ccd
Ziroll <jsparlingzi3757@gmail.com>
2024-06-29 12:57:08 -0500

    Defined columns, and setup associations.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category);
+Product.belongsTo(Category);
+
+// team=category
+// player=Product
 
 // Categories have many Products
-
+Category.hasMany(Product);
+Product.belongsTo(Category);
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
+Product.belongsToMany(Tag, { through: 'ProductTag' });
+Tag.belongsToMany(Product, { through: 'ProductTag' });
+
+// Actor = product
+// movies = tag
 
 module.exports = {
   Product,
